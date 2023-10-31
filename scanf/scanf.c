8b63058b905ef48d18073a5c2bf44b132abf7185
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2023-10-31 19:24:15 -0300

    scanf

---
+++ scanf/scanf.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+
+int main(void){
+    int A;
+    int B;
+    int C;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    C = A + B;
+
+    printf("%d", C);
+
+    return 0;
+}
