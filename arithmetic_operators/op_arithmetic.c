33d5c7571bcad02fb1d8474e41d74d0c7ab84447
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2023-10-31 19:40:53 -0300

    arithmetic operators

---
+++ arithmetic_operators/op_arithmetic.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+
+int main(void){
+    int x, y;
+    x = 12; y = 4;
+
+    printf("%d\n", x/y);
+    printf("%d\n", x%y);
+    printf("%d\n", x*y);
+    printf("%d\n", x+y);
+    printf("%d\n", x-y);
+}
