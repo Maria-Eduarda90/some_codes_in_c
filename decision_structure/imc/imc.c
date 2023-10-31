35156715089871719fe948d030678e797ef5a0eb
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2023-10-31 20:49:40 -0300

    imc

---
+++ decision_structure/imc/imc.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+#include<math.h>
+
+int main(void){
+    float p, a, i;
+
+    printf("Peso? ");
+    scanf("%f", &p);
+
+    printf("Altura? ");
+    scanf("%f", &a);
+
+    i = p / pow(a, 2);
+
+    printf("IMC = %.2f\n", i);
+
+    if(i > 30.0) printf("Obeso\n");
+
+    else printf("Normal\n");
+
+    return 0;
+}
